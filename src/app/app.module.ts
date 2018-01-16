98380b06acd5f02e225bfbc91adf72b517ff1d25
Jesse Saarimaa <jessesaa@ety-a2123b-03.edu.wks.metropolia.fi>
2018-01-16 11:41:41 +0200

    moi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { MaincontentComponent } from './maincontent/maincontent.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    FooterComponent,
+    MaincontentComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
